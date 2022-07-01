bf986116f9283f4248aef0ce445c501e53ea23d3
Carlos Gonzalez Oliver <cgonzalez@bs-mbpr502.d.ethz.ch>
2022-07-01 18:18:06 +0200

    lets see

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,66 +1,41 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import datetime
 
-# -- Path setup --------------------------------------------------------------
+import torch_geometric
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../../'))
-
-
-# -- Project information -----------------------------------------------------
-
+author = "Kucera, Oliver, O'Bray, Chen"
 project = 'torch-pdb'
-copyright = "2022, Tim Kucera, Carlos Oliver, Leslie O'Bray, Dexiong Chen"
-author = "Tim Kucera, Carlos Oliver, Leslie O'Bray, Dexiong Chen"
-
-# The full version, including alpha/beta/rc tags
-release = '0.0.1'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+version = torch_pdb.__version__
+copyright = f'{datetime.datetime.now().year}, {author}'
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
 
-napoleon_google_docstring = False
-napoleon_use_param = False
-napoleon_use_ivar = True
+html_theme = 'pyg_sphinx_theme'
+html_logo = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
+             'master/pyg_sphinx_theme/static/img/pyg_logo.png')
+html_favicon = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
+                'master/pyg_sphinx_theme/static/img/favicon.png')
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+add_module_names = False
+autodoc_member_order = 'bysource'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy', None),
+    'pandas': ('http://pandas.pydata.org/pandas-docs/dev', None),
+    'torch': ('https://pytorch.org/docs/master', None),
+}
 
 
-# -- Options for HTML output -------------------------------------------------
+def setup(app):
+    def rst_jinja_render(app, _, source):
+        rst_context = {'torch_pdb': torch_pdb}
+        source[0] = app.builder.templates.render_string(source[0], rst_context)
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+    app.connect('source-read', rst_jinja_render)
