0892a868ab006a4fb43f3e11661c988ce25236bf
Tim Kucera <timkucera@gmail.com>
2022-07-13 18:00:54 +0200

    util docstrings

--- torch_pdb/utils/embeddings.py
+++ torch_pdb/utils/embeddings.py
@@ -1,3 +1,9 @@
+"""
+Some basic functions for embedding protein sequences. Supply to a representation class.
+
+All embeddings take a sequence string as an input and return the embedding as a numpy array of shape n x d.
+"""
+
 import numpy as np
 
 alphabet = 'ARNDCEQGHILKMFPSTWYV'
@@ -5,10 +11,15 @@ alphabet = 'ARNDCEQGHILKMFPSTWYV'
 def one_hot(sequence):
     """ Compute the one-hot encoding of a protein sequence.
 
-    Args:
-        sequence:
+    Parameters
+    ----------
+    sequence: str
+        The protein sequence
 
-    Returns:
+    Returns
+    -------
+    ndarray
+        The embedded sequence.
     """
 
     return np.stack([np.eye(len(alphabet))[alphabet.index(aa)] for aa in sequence])
