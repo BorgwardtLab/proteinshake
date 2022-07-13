0017247ac2783e82468b344e2b4cab95abd7c2a3
Tim Kucera <timkucera@gmail.com>
2022-07-13 15:15:02 +0200

    release.py comments

--- scripts/release.py
+++ scripts/release.py
@@ -1,3 +1,19 @@
+'''
+Script to generate a dataset release for hosting. It runs all available datasets with 'use_precomputed=False'. If a scratch location (--scratch) is passed (default), the raw dataset will be downloaded to the scratch and afterwards copied to the target folder (--path).
+
+How to generate a new release:
+1. run release.py
+2. make a new release on GitHub, attach the .json.gz file of each dataset
+3. tag it after date of download, like '12JUL2022'
+4. change the default argument 'release' in torch_pdb.datasets.dataset.TorchPDBDataset to the new release tag
+
+How to add a new dataset to the release pipeline:
+1. import the dataset class
+2. add it to the first loop
+3. run release
+
+'''
+
 import os, shutil, argparse
 from torch_pdb.datasets import PDBBindRefined, PDBBindPPI, TMScoreBenchmark, GODataset, ECDataset, PfamDataset, RCSBDataset
 
@@ -11,26 +27,39 @@ PATH = args.path
 SCRATCH = args.scratch if args.scratch != '' else args.path
 n_jobs = args.njobs
 
+
+###################
+# PDB Datasets
+###################
 os.makedirs(f'{PATH}/release', exist_ok=True)
 for Dataset in [RCSBDataset, ECDataset, GODataset, PfamDataset, PDBBindRefined, PDBBindPPI, TMScoreBenchmark]:
+    # name it after class name
     name = Dataset.__name__
     print()
     print(name)
+    # create dataset
     ds = Dataset(root=f'{SCRATCH}/release/{name}', use_precomputed=False, n_jobs=n_jobs)
     print('Length:', len(ds.proteins))
+    # delete to free memory
     del ds
+    # copy from scratch to target
     if SCRATCH != PATH and not os.path.exists(f'{PATH}/release/{name}.json.gz'):
         print('Copying...')
         shutil.copyfile(f'{SCRATCH}/release/{name}/{name}.json.gz', f'{PATH}/release/{name}.json.gz')
 
+# copy the extra file (pairwise distances) from the TM dataset
 if SCRATCH != PATH and not os.path.exists(f'{PATH}/release/tmalign.json.gz'):
     print('Copying TM scores...')
     shutil.copyfile(f'{SCRATCH}/release/TMScoreBenchmark/tmalign.json.gz', f'{PATH}/release/tmalign.json.gz')
 
 
+###################
+# AlphaFold Datasets
+###################
 from torch_pdb.datasets.alphafold import AF_DATASET_NAMES
 from torch_pdb.datasets import AlphaFoldDataset
 
+# create one dataset for each organism
 for organism in AF_DATASET_NAMES.keys():
     print()
     print('AlphaFoldDataset', organism)
