dd3d34105609798b4f9a1ad0691b17fe8c61327c
Carlos Gonzalez Oliver <cgonzalez@bs-mbpr502.d.ethz.ch>
2022-08-12 14:46:50 +0200

    artwork change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,9 @@ extensions = [
 
 html_theme = 'pyg_sphinx_theme'
 html_logo = ('https://raw.githubusercontent.com/cgoliver/torch-pdb_sphinx_theme/'
-             'master/pyg_sphinx_theme/static/img/torch-pdb.png')
+             'master/pyg_sphinx_theme/static/img/proteinshake.png')
 html_favicon= ('https://raw.githubusercontent.com/cgoliver/torch-pdb_sphinx_theme/'
-             'master/pyg_sphinx_theme/static/img/favicon.png')
+             'master/pyg_sphinx_theme/static/img/favicon.ico')
 
 add_module_names = False
 autodoc_member_order = 'bysource'
