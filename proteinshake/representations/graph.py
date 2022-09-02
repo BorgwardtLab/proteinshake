989ee38df6fce29b63f6ab28d2a226df8defd6c1
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2022-09-02 16:35:33 +0200

    move dgl dataset class outside of GraphDataset class

--- proteinshake/representations/graph.py
+++ proteinshake/representations/graph.py
@@ -92,34 +92,11 @@ class GraphDataset():
                        pre_filter=pre_filter)
 
     def dgl(self):
-        raise NotImplementedError
+        from .dgl_data import Dataset
 
-    def dgl(self):
-        from dgl.data import DGLDataset
-        from dgl import save_graphs, load_graphs
-        import dgl
-        import torch
-        def graph2dgl(graph, info={}):
-            g = dgl.from_scipy(graph[1])
-            for key,value in info.items():
-                if type(value) == list and len(value) == len(info['sequence']):
-                    try:
-                        g.ndata[key] = torch.tensor(value)
-                    except:
-                        pass
-            return g
-        class Dataset(DGLDataset):
-            def __init__(self, path, proteins, info):
-                if os.path.exists(path):
-                    self.proteins = load_graphs(path)
-                else:
-                    self.proteins = [graph2dgl(p, info=info) for p,info in zip(proteins,info)]
-                    save_graphs(path, self.proteins)
-            def __getitem__(self, i):
-                    return self.proteins[i]
-            def __len__(self):
-                    return len(self.proteins)
-        ds = Dataset(f'{self.root}/processed/graph/{self.name}.dgl.pkl',  self.proteins, self.info)
+        ds = Dataset(f'{self.root}/processed/graph/{self.name}.dgl.pkl',
+                     self.proteins,
+                     self.info)
         return ds
 
         @checkpoint('{root}/processed/graph/{name}.nx.pkl')
