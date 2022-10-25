628b4d6e995f5c6d6190074c5af63fa9ac06e42f
Carlos Gonzalez Oliver <cgonzalez@bs-mbpr502.d.ethz.ch>
2022-10-25 17:50:54 +0200

    init import

--- proteinshake/tasks/__init__.py
+++ proteinshake/tasks/__init__.py
@@ -1,10 +1,12 @@
 from .task import ShakeTask
 from .ec_task import EnzymeCommissionTask
 from .ligand_affinity_task import LigandAffinityTask
+from .tm_task import RetrieveTask
 
 classes = ['ShakeTask',
            'EnzymeCommissionTask',
-           'LigandAffinityTask'
+           'LigandAffinityTask',
+           'RetrieveTask'
            ]
 
 __all__ = classes
