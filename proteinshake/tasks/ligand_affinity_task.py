6df2eb428d8aee678b6b80fd5e18e1ec2dd0e060
Carlos Gonzalez Oliver <cgonzalez@bs-mbpr502.d.ethz.ch>
2022-10-25 17:52:29 +0200

    kill pyg y thing

--- proteinshake/tasks/ligand_affinity_task.py
+++ proteinshake/tasks/ligand_affinity_task.py
@@ -11,10 +11,3 @@ class LigandAffinityTask(ShakeTask):
 
     def evaluator(self):
         return metrics.mean_squared_error
-
-    def set_target_pyg(self, pyg_dataset, batch_size=32, framework='pyg'):
-        d = []
-        for p in pyg_dataset:
-            p.y = getattr(p, self.target)
-            d.append(p)
-        return d
