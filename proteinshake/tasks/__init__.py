d3eaee6e87ba95d1a35ef6126094c6ec768de8a3
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2022-11-14 11:46:16 +0100

    fix binding site import

--- proteinshake/tasks/__init__.py
+++ proteinshake/tasks/__init__.py
@@ -1,12 +1,14 @@
 from .task import ShakeTask
 from .ec_task import EnzymeCommissionTask
 from .ligand_affinity_task import LigandAffinityTask
+from .binding_site_task import BindingSitePredictionTask
 from .tm_task import RetrieveTask
 
 classes = ['ShakeTask',
            'EnzymeCommissionTask',
            'LigandAffinityTask',
-           'RetrieveTask'
+           'RetrieveTask',
+           'BindingSitePredictionTask'
            ]
 
 __all__ = classes
