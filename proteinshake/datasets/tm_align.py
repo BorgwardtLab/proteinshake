01caf23cae3d988ead43671ace2bb24279f2a445
timkucera <timkucera@gmail.com>
2022-11-15 09:58:42 +0100

    include tmalign in setup

--- proteinshake/datasets/tm_align.py
+++ proteinshake/datasets/tm_align.py
@@ -15,7 +15,7 @@ from proteinshake.utils import extract_tar, download_url, save, load, unzip_file
 
 # short-term absolute path hack for TMalign
 # we need to include this with the setuptools
-TMPATH = os.path.dirname(os.path.realpath(__file__))+'/../../TMalign'
+TMPATH = os.path.dirname(os.path.realpath(__file__))+'/../../tmalign/TMalign'
 
 def tmalign_wrapper(pdb1, pdb2):
     """Compute TM score with TMalign between two PDB structures.
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Extension
 
 __version__ = '0.1.0'
 URL = 'https://proteinshake.readthedocs.io/en/latest/index.html'
@@ -20,11 +20,17 @@ test_requires = [
     'pytest',
 ]
 
+tmalign = Extension('TMalign',
+    sources = ['tmalign/TMalign.cpp'],
+    language = 'c',
+    extra_compile_args = ['-static','-O3','-ffast-math','-lm']
+)
+
 setup(
     name='proteinshake',
     version=__version__,
     description='Deep learning ready datasets of 3D protein structures.',
-    author = "Tim Kucera, Carlos Oliver, Leslie O'Bray, Dexiong Chen, Karsten Borgwardt",
+    author = "Tim Kucera, Carlos Oliver, Dexiong Chen, Karsten Borgwardt",
     author_email = "tim.kucera@bsse.ethz.ch",
     url=URL,
     keywords = ['bioinformatics',
@@ -37,4 +43,5 @@ setup(
     install_requires=install_requires,
     packages=find_packages(),
     include_package_data=True,
+    ext_modules=[tmalign]
 )
