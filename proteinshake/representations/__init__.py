ba89844943ffb0d5d47075b3685504832bea1967
Carlos <carlos@ozeki.io>
2022-12-05 16:38:40 +0100

    init fix

--- proteinshake/representations/__init__.py
+++ proteinshake/representations/__init__.py
@@ -1,14 +1,12 @@
 from .graph import GraphDataset
 from .point import PointDataset
 from .voxel import VoxelDataset
-from .surface import SurfaceDataset
 
 
 __all__ = [
     'GraphDataset',
     'PointDataset',
     'VoxelDataset',
-    'SurfaceDataset'
     ]
 
 classes = __all__
