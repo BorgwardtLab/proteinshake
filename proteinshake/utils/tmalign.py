f4fb9b8e30a61ffe9e2d91ce54aaaafc2506897f
Tim Kucera <timkucera@gmail.com>
2023-02-06 18:58:42 +0100

    switch back to full similarity matrix

--- proteinshake/utils/tmalign.py
+++ proteinshake/utils/tmalign.py
@@ -3,7 +3,19 @@ import os
 import subprocess
 import tempfile
 from biopandas.pdb import PandasPdb
-from .gdt import gdt
+from scipy.spatial.distance import cdist
+import numpy as np
+
+def global_distance_test(superposition):
+    df = superposition.df['ATOM']
+    df = df[df['atom_name'] == 'CA']
+    A = df[df['chain_id'] == 'A']
+    B = df[df['chain_id'] == 'B']
+    coordsA = np.array(list(zip(A['x_coord'], A['y_coord'], A['z_coord'])))
+    coordsB = np.array(list(zip(B['x_coord'], B['y_coord'], B['z_coord'])))
+    dist = np.sqrt(((coordsA-coordsB)**2).sum(1))
+    thresholds = [1,2,4,8]
+    return np.mean([np.mean(dist<=t) for t in thresholds])
 
 def tmalign_wrapper(pdb1, pdb2):
     """Compute TM score with TMalign between two PDB structures.
@@ -11,18 +23,14 @@ def tmalign_wrapper(pdb1, pdb2):
     ----------
     pdb1: str
         Path to PDB.
-    arg2 : str
+    pdb2 : str
         Path to PDB.
     return_superposition: bool
         If True, returns a protein dataframe with superposed structures.
     Returns
     -------
-    float
-        TM score from `pdb1` to `pdb2`
-    float
-        TM score from `pdb2` to `pdb1`
-    float
-        RMSD between structures
+    dict
+        Metric values TM1/TM2 (TM-Scores normalized to pdb1 or pdb2), RMSD, GDT
     """
     assert shutil.which('TMalign') is not None,\
            "No TMalign installation found. Go here to install : https://zhanggroup.org/TM-align/TMalign.cpp"
@@ -30,7 +38,7 @@ def tmalign_wrapper(pdb1, pdb2):
         out = subprocess.run(['TMalign','-outfmt','2', pdb1, pdb2, '-o', os.path.join(tmpdir, 'superposition.pdb')], stdout=subprocess.PIPE).stdout.decode()
         superposition = PandasPdb().read_pdb(os.path.join(tmpdir, 'superposition.pdb')) 
     path1, path2, TM1, TM2, RMSD, ID1, ID2, IDali, L1, L2, Lali = out.split('\n')[1].split('\t')
-    GDT = gdt(superposition)
+    GDT = global_distance_test(superposition)
     return {
         'TM1': float(TM1),
         'TM2': float(TM2),
