3768d4ce2fe46f45763ad4d66f0303458b0e88cf
Tim Kucera <timkucera@gmail.com>
2023-03-15 18:21:42 +0100

    speed up transforms test

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,24 +1,34 @@
 import unittest, tempfile
 
-from proteinshake.datasets import ProteinLigandDecoysDataset as dset
+from proteinshake.datasets import AlphaFoldDataset
 from proteinshake.transforms import *
 
 
 class TestTransforms(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(self):
+        organism = 'methanocaldococcus jannaschii'
+        self.tmpdir = tempfile.TemporaryDirectory()
+        self.ds = AlphaFoldDataset(root=self.tmpdir, organism=organism)
+
+    @classmethod
+    def tearDownClass(self):
+        self.tmpdir.cleanup()
+
     def test_identity(self):
         with tempfile.TemporaryDirectory() as tmp:
-            da = dset(root=tmp).to_voxel(transform=IdentityTransform())
+            self.ds.to_voxel(transform=IdentityTransform())
 
     def test_center(self):
         with tempfile.TemporaryDirectory() as tmp:
-            da = dset(root=tmp).to_voxel(transform=CenterTransform())
+            self.ds.to_voxel(transform=CenterTransform())
 
     def test_rotate(self):
         with tempfile.TemporaryDirectory() as tmp:
-            da = dset(root=tmp).to_voxel(transform=RandomRotateTransform())
+            self.ds.to_voxel(transform=RandomRotateTransform())
 
     def test_compose(self):
         t = Compose([CenterTransform(), RandomRotateTransform()])
         with tempfile.TemporaryDirectory() as tmp:
-            da = dset(root=tmp).to_voxel(transform=t)
+            self.ds.to_voxel(transform=t)
