ad17fd5cc55fa787ff1e0ce50ede882300d2dfb4
Tim Kucera <timkucera@gmail.com>
2023-03-23 09:37:03 +0100

    bump zenodo release

--- proteinshake/datasets/dataset.py
+++ proteinshake/datasets/dataset.py
@@ -24,7 +24,7 @@ AA_ONE_TO_THREE = {v:k for k, v in AA_THREE_TO_ONE.items()}
 
 # maps the date-format release to Zenodo identifier
 RELEASES = {
-    'latest': '1174461',
+    'latest': '1175739',
 }
 
 class Dataset():
